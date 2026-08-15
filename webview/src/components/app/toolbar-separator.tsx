b51db258aa40046f43d6c23fa5aa0703a5c18131
Sebastien Guisse <sebastien.guisse@decathlon.com>
2026-08-15 11:53:44 +0200

    save

--- webview/src/components/app/toolbar-separator.tsx
+++ webview/src/components/app/toolbar-separator.tsx
@@ -11,14 +11,14 @@ export function ToolbarSeparator({ orientation = 'VERTICAL', className = '' }: T
   if (orientation === 'VERTICAL') {
     return (
       <div className={`flex items-center justify-center h-full w-3 select-none ${className}`}>
-        <div className="bg-border w-[1px] h-4" />
+        <div className="bg-slate-400 dark:bg-slate-500 w-[1px] h-4" />
       </div>
     );
   }
 
   return (
     <div className={`flex items-center justify-center w-5 py-1.5 shrink-0 select-none ${className}`}>
-      <div className="bg-border w-2 h-[1px] shrink-0" />
+      <div className="bg-slate-400 dark:bg-slate-500 w-2 h-[1px] shrink-0" />
     </div>
   );
 }
